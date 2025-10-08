957e0df96bd341f11ac0b6ac746e581bbc08a743
Pedro Zucchi <predozucchi@gmail.com>
2025-10-08 11:00:38 -0300

    changing seed config

--- scripts/seed-database.js
+++ scripts/seed-database.js
@@ -1,6 +1,6 @@
 const { MongoClient, ObjectId } = require("mongodb");
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ecotech_dataflow";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const usuarios = [
   {
